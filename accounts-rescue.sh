0060808566f16017090667d016d9d347eaf86d14
nmartins0611 <62961138+nmartins0611@users.noreply.github.com>
2022-04-03 14:11:27 +0200

    Moved into role

--- accounts-rescue.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -v -k -s -i -X POST -H 'Content-Type:application/json' --data '{"host_config_key": "cfbaae23-81c0-47f8-9a40-44493b82f06a"}' https://controller.nostromo.co.za/api/v2/job_templates/20/callback/
